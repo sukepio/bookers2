d8acbfc135e7a24236b11174d0df598f395115fd
sukepio <naoto927sukena@gmail.com>
2020-12-16 00:27:47 +0000

    [Add] Post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/about' => 'homes#about'
   devise_for :users
   root to: 'homes#top'
-  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
