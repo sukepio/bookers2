8550152e9799896cbb2b5666b4621e3188e7e3fe
sukepio <naoto927sukena@gmail.com>
2020-12-16 00:41:06 +0000

    [Add] post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
+  resources :post, only: [:create, :index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
