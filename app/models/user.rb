df20ee0fbdf8b15e61998ef8cdd36fc8059acf45
sukepio <naoto927sukena@gmail.com>
2020-12-29 13:06:52 +0000

    [add] いいね機能 途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
   
   validates :name, uniqueness: true, length: {minimum: 2, maximum: 20}
   validates :introduction, length: {maximum: 50}
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
-    resources :post_comments, only: [:create, :destroy
-    ]
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
