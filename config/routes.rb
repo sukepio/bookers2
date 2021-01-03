0a53d33d2ec2e92a7d2b5ccce228827caf53f251
sukepio <naoto927sukena@gmail.com>
2021-01-03 00:57:56 +0000

    [Add] 検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   post 'follow/:id' => 'relationships#follow', as: 'follow'
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get'search' => 'searches#search'
+  
 end
