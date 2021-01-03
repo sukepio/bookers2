421b1542130550b381b569947bd47c8bdbc99bc9
sukepio <naoto927sukena@gmail.com>
2021-01-03 03:02:50 +0000

    [add] partial for コメント

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,11 +1,12 @@
 class PostCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    @books = Book.new
+    @post_comment = PostComment.new
   end
   
   def destroy
